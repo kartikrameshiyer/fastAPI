691c4da27427ead49a2073dcd42fc68f9efb4408
kartik <kartik.ramesh@icloud.com>
2023-11-14 17:53:50 -0500

    pydantic scgema, db util

--- api/users.py
+++ api/users.py
@@ -1,25 +1,26 @@
-import fastapi
-from typing import Optional,List
-from pydantic import BaseModel
-from fastapi import Path
 
+from typing import Optional,List
+
+import fastapi
+from sqlalchemy.orm import session
+from fastapi import Path,Depends,HTTPException
+
+from api.utils.users import get_user,get_user_by_email,create_user,get_users
+from db.db_setup import get_db
+from pydantic_schemas.user import UserCreate, User
 
 router = fastapi.APIRouter()
 
-users=[]
-
-class User(BaseModel):
-    email : str
-    is_active : bool
-    bio : Optional[str] = None
     
 @router.get("/users",response_model=List[User])
-async def get_users():
+async def read_users( skip: int = 0, limit: int = 100, db: session = Depends(get_db)):
+    users = get_users(db,skip=skip, limit = limit)
     return users
+
 @router.post("/users")
-async def create_user(user: User):
-    users.append(user)
-    return "success" 
-@router.get("/user/{id}")
-async def get_user(id: int= Path(...,description=" The iD of the user you want to retreive", gt=2)):
-    return users[id]
+async def create_new_user(user : UserCreate,db: session = Depends(get_db)):
+    return create_user(db=db,user=user)
+
+@router.get("/user/{user_id}")
+async def read_user(user_id : int = Path(...,description=" The iD of the user you want to retreive"),db: session = Depends(get_db)):
+    return get_user(db=db,user_id=user_id)
